3bccc45cd8bd41336d394f81ff706685014ed943
Yu Kit Foo <2441458f@student.gla.ac.uk>
2021-01-15 18:57:44 +0000

    third commit

--- rango/views.py
+++ rango/views.py
@@ -3,4 +3,9 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    context_dict = {"boldmessage":"Crunchy, creamy, cookie, candy, cupcake!"}
+    return render(request, 'rango/index.html', context=context_dict)
+
+def about(request):
+    context_dict = {"boldmessage":"This tutorial has been put together by Yu Kit"}
+    return render(request, 'rango/about.html', context=context_dict)
