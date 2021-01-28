bf1dff2185d435075faca5afed78d828fda78ac5
Yu Kit Foo <2441458f@student.gla.ac.uk>
2021-01-28 21:46:07 +0000

    seventh commit

--- rango/views.py
+++ rango/views.py
@@ -20,7 +20,7 @@ def index(request):
 
 def about(request):
     context_dict = {"boldmessage":"This tutorial has been put together by Yu Kit"}
-    return render(request, 'rango/about.html', context=context_dict)
+    return render(request, "rango/about.html", context=context_dict)
 
 
 def show_category(request, category_name_slug):
